14a143d6441b706536debd377877ce1e0b78c8c9
Helen Hood <helen.c.hood@gmail.com>
2014-07-21 15:34:19 -0400

    Move namespacing down in file

--- app/assets/javascripts/store.js
+++ app/assets/javascripts/store.js
@@ -1,7 +1,3 @@
-DS.RESTAdapter.reopen({
-  namespace: 'api/v1'
-});
-
 App.Store = DS.Store.extend({
 
 });
@@ -10,4 +6,8 @@ App.Store = DS.Store.extend({
 // is built to work nicely with the ActiveModel::Serializers gem.
 App.ApplicationAdapter = DS.ActiveModelAdapter.extend({
 
+});
+
+DS.RESTAdapter.reopen({
+  namespace: 'api/v1'
 });
