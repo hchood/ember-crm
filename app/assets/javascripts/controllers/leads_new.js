523ac77382cfe3f115c57e224ad260c8b5e80bc1
Helen Hood <helen.c.hood@gmail.com>
2014-07-24 16:17:55 -0400

    Add new leads controller w/ createLead action

---
+++ app/assets/javascripts/controllers/leads_new.js
@@ -0,0 +1,11 @@
+App.LeadsNewController = Ember.Controller.extend({
+  actions: {
+    createLead: function() {
+      var self = this;
+      var lead = this.store.createRecord('lead', this.get('fields'));
+      lead.save().then(function() {
+        self.transitionToRoute('lead', lead);
+      });
+    }
+  }
+});
