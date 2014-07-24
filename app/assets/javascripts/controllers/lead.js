3cd09daa17dac61e329ef61a5b62f0698d322d17
Helen Hood <helen.c.hood@gmail.com>
2014-07-24 11:31:24 -0400

    Fix so unsaved message doesn't show when record is saving

--- app/assets/javascripts/controllers/lead.js
+++ app/assets/javascripts/controllers/lead.js
@@ -1,4 +1,8 @@
 App.LeadController = Ember.ObjectController.extend({
+  showUnsavedMessage: function() {
+    return this.get('isDirty') && !this.get('isSaving');
+  }.property('isDirty', 'isSaving'),
+
   actions: {
     saveChanges: function() {
       if (this.get('model.isDirty')) this.get('model').save();
