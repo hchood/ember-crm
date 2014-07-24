ba379b6223c10b4a8a1ee240c9110c107eada76c
Helen Hood <helen.c.hood@gmail.com>
2014-07-24 15:25:29 -0400

    Delete lead

--- app/assets/javascripts/controllers/lead.js
+++ app/assets/javascripts/controllers/lead.js
@@ -8,6 +8,13 @@ App.LeadController = Ember.ObjectController.extend({
   actions: {
     saveChanges: function() {
       if (this.get('model.isDirty')) this.get('model').save();
+    },
+
+    delete: function() {
+      var self = this;
+      this.get('model').destroyRecord().then(function() {
+        self.transitionToRoute('leads');
+      });
     }
   }
 });
