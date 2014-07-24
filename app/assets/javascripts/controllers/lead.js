5e75dd1d7a5cb93b4368fbf7b9606084bde9cc9c
Helen Hood <helen.c.hood@gmail.com>
2014-07-24 13:51:52 -0400

    Add isEditing property on LeadController for clarity

--- app/assets/javascripts/controllers/lead.js
+++ app/assets/javascripts/controllers/lead.js
@@ -1,4 +1,6 @@
 App.LeadController = Ember.ObjectController.extend({
+  isEditing: false,
+
   showUnsavedMessage: function() {
     return this.get('isDirty') && !this.get('isSaving');
   }.property('isDirty', 'isSaving'),
