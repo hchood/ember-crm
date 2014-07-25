4155d164e042d19e49532fd63b3a36d17fa2e788
Helen Hood <helen.c.hood@gmail.com>
2014-07-25 09:59:04 -0400

    Modify controller to return leads that match search term

--- app/assets/javascripts/controllers/leads.js
+++ app/assets/javascripts/controllers/leads.js
@@ -1,3 +1,18 @@
 App.LeadsController = Ember.ArrayController.extend({
-  sortProperties: ['firstName', 'lastName']
+  sortProperties: ['firstName', 'lastName'],
+
+  //  leads property looks for search string.  if it exists, returns
+  //  searchedLeads, otherwise returns array of models it's wrapping.
+  leads: function() {
+    return this.get('search') ? this.get('searchedLeads') : this;
+  }.property('search', 'searchedLeads'),
+
+  // runs filter on the array of leads & returns leads where the
+  // full name includes the search string.
+  searchedLeads: function() {
+    var search = this.get('search').toLowerCase();
+    return this.filter(function(lead) {
+      return lead.get('fullName').toLowerCase().indexOf(search) != -1;
+    });
+  }.property('search', 'this.@each.fullName')
 });
